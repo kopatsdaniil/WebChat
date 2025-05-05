c07669c114ddd25e06938dac09203484b285b131
daniilkopot <kopot.dan@gmail.com>
2025-05-05 11:40:02 +0300

    feat(dev): add program.cs with swagger

--- WebChat/backend/Program.cs
+++ WebChat/backend/Program.cs
@@ -1,6 +1,25 @@
+using backend.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapControllers();
 
 app.Run();
