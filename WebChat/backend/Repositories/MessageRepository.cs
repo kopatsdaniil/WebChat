a5dd92980fbcaa35e9cf98d77059b67ad6c9fa35
daniilkopot <kopot.dan@gmail.com>
2025-05-05 11:57:00 +0300

    feat(dev): add repositories for all entities

---
+++ WebChat/backend/Repositories/MessageRepository.cs
@@ -0,0 +1,58 @@
+using backend.Data;
+using backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Repositories;
+
+public class MessageRepository(ApplicationDbContext context)
+{
+    public async Task<List<Message>?> GetAllAsync()
+    {
+        return await context.Messages.ToListAsync();
+    }
+
+    public async Task<List<Message>?> GetBySenderIdAsync(Guid senderId)
+    {
+        return await context.Messages.Where(m => m.SenderId == senderId).ToListAsync();
+    }
+
+    public async Task<Message?> GetByIdAsync(Guid id)
+    {
+        return await context.Messages.FirstOrDefaultAsync(m => m.Id == id);
+    }
+
+    public async Task<Message> CreateAsync(Message message)
+    {
+        await context.Messages.AddAsync(message);
+        await context.SaveChangesAsync();
+        
+        return message;
+    }
+
+    public async Task<Message?> UpdateAsync(Guid id, string text, DateTime date, Guid senderId)
+    {
+        var message = await GetByIdAsync(id);
+        
+        if (message == null) throw new KeyNotFoundException("Message not found");
+        
+        message.Text = text;
+        message.Date = date;
+        message.SenderId = senderId;
+        
+        await context.SaveChangesAsync();
+        
+        return message;
+    }
+
+    public async Task<Message?> DeleteAsync(Guid id)
+    {
+        var message = await GetByIdAsync(id);
+        
+        if (message == null) throw new KeyNotFoundException("Message not found");
+        
+        context.Messages.Remove(message);
+        await context.SaveChangesAsync();
+        
+        return message;
+    }
+}
---
+++ WebChat/backend/Repositories/UserRepository.cs
@@ -0,0 +1,52 @@
+using backend.Data;
+using backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Repositories;
+
+public class UserRepository(ApplicationDbContext context)
+{
+    public async Task<List<User>?> GetAllAsync()
+    {
+        return await context.Users.ToListAsync();
+    }
+
+    public async Task<User?> GetByIdAsync(Guid id)
+    {
+        return await context.Users.FirstOrDefaultAsync(x => x.Id == id);
+    }
+
+    public async Task<User> CreateAsync(User user)
+    {
+        await context.Users.AddAsync(user);
+        await context.SaveChangesAsync();
+        
+        return user;
+    }
+
+    public async Task<User?> UpdateAsync(Guid id, string username, string passwordHash)
+    {
+        var user = await GetByIdAsync(id);
+        
+        if(user == null) throw new KeyNotFoundException("User not found");
+        
+        user.Username = username;
+        user.PasswordHash = passwordHash;
+        
+        await context.SaveChangesAsync();
+        
+        return user;
+    }
+
+    public async Task<User?> DeleteAsync(Guid id)
+    {
+        var user = await GetByIdAsync(id);
+        
+        if(user == null) throw new KeyNotFoundException("User not found");
+
+        context.Users.Remove(user);
+        await context.SaveChangesAsync();
+        
+        return user;
+    }
+}
